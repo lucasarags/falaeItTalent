dbe4367c6e0dca6d1bd8c34480a84d3d37703766
Lucas Aragao Alves da Costa <30377854+lucasarags@users.noreply.github.com>
2023-11-21 15:22:17 -0300

    Update restoreDB.sh

--- restoreDB.sh
+++ restoreDB.sh
@@ -1,17 +1,19 @@
-# USER="root"
-# HOST="localhost"
-# PASS="falae@pass"
-# DBNAME="falae"
+# restoredb.sh
 
-if [ -z "$*" ] ; then
-    echo 'Use restore [arquivo-dump.sql]'
-    exit 0
+#!/bin/bash
+
+# Define as variáveis de ambiente
+DB_USER=root
+DB_PASSWORD=falae@pass
+DB_NAME=falae
+DB_HOST=db
+DB_PORT=3306
+
+# Verifica se foi fornecido um arquivo SQL como argumento
+if [ -z "$1" ]; then
+    echo "Use: $0 [arquivo-dump.sql]"
+    exit 1
 fi
 
-echo "Importing DB..."
-# docker exec -i mysql sh -c 'exec mysql -u$DB_USER -p"$DB_PASSWORD"' < $1
-# docker exec -i mysql sh -c 'exec mysql -u$DB_USER -p"$DB_PASSWORD" -h$DB_HOST -P$DB_PORT $DB_NAME' < $1
-# docker exec -i mysql sh -c 'exec mysql -u"$DB_USER" -p"$DB_PASSWORD"' < $1
-docker exec -i mysql sh -c 'exec mysql -u"$DB_USER" -p"$DB_PASSWORD" -hdb -P"$DB_PORT" "$DB_NAME"' < $1
-
-
+echo "Importando o banco de dados..."
+docker exec -i mysql sh -c "exec mysql -u$DB_USER -p'$DB_PASSWORD' -h$DB_HOST -P$DB_PORT $DB_NAME" < "$1"
